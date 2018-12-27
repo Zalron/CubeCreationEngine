653c85a3605cc8938e527f8978aa85ac5f8ff243
Zalron <33308793+Zalron@users.noreply.github.com>
2018-12-27 20:33:13 +1100

    Added logic and ui that allows you to hit the play button and watch a loading bar fill up
    So that we can see how the terrain generation is progressing for the first time the play hits the play button

--- CubeCreationEngineUnity/Assets/Scripts/World.cs
+++ CubeCreationEngineUnity/Assets/Scripts/World.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 namespace CubeCreationEngine.Core
 {
     public class World : MonoBehaviour
     {
         public GameObject player;
         public Material textureAtlas; // the texture that is going to be aplided to the chunks
+        public Slider loadingAmount;
+        public Camera cam;
+        public Button playButton;
         public static int columnHeight = 16; // the height of the world
         public static int chunkSize = 16; // the size of the chunk
         public static int worldSize = 2; // size of the world
-        public static int radus = 3;
+        public static int radius = 3;
         public static Dictionary<string, Chunk> chunks; // a disctionary of all of the chunks
         public static string BuildChunkName(Vector3 v) // assigning a name to a chunk
         {
@@ -35,25 +40,40 @@ namespace CubeCreationEngine.Core
         {
             int posx = (int)Mathf.Floor(player.transform.position.x / chunkSize);
             int posz = (int)Mathf.Floor(player.transform.position.z / chunkSize);
-            for (int x = -radus; x < radus; x++)
+            float totalChunks = (Mathf.Pow(radius * 2 + 1, 2) * columnHeight) * 2;
+            int processCount = 0;
+            for (int x = -radius; x < radius; x++)
             {
                 for (int y = 0; y < columnHeight; y++)
                 {
-                    for (int z = -radus; z < radus; z++)
+                    for (int z = -radius; z < radius; z++)
                     {
                         Vector3 chunkPosition = new Vector3((x+posx) * chunkSize, y * chunkSize, (z + posz) * chunkSize);
                         Chunk c = new Chunk(chunkPosition, textureAtlas);
                         c.chunk.transform.parent = this.transform;
                         chunks.Add(c.chunk.name, c);
+                        processCount++;
+                        loadingAmount.value = processCount / totalChunks * 100;
+                        yield return null;
                     }
                 }
             }
             foreach (KeyValuePair<string, Chunk> c in chunks)
             {
                 c.Value.DrawChunk();
+                processCount++;
+                loadingAmount.value = processCount / totalChunks * 100;
                 yield return null;
+
             }
             player.SetActive(true);
+            loadingAmount.gameObject.SetActive(false);
+            cam.gameObject.SetActive(false);
+            playButton.gameObject.SetActive(false);
+        }
+        public void StartBuild() // the function that is called when the player presses the play button
+        {
+            StartCoroutine(BuildWorld());
         }
         void Start() // Use this for initialization
         {
@@ -61,7 +81,6 @@ namespace CubeCreationEngine.Core
             chunks = new Dictionary<string, Chunk>();
             this.transform.position = Vector3.zero;
             this.transform.rotation = Quaternion.identity;
-            StartCoroutine(BuildWorld());
         }
         void Update()// Update is called once per frame
         {
